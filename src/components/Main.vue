48a014ebc3af7731c9ac89340b4d58f61724a37b
kenpeter <figo2476@hotmail.com>
2017-03-14 15:57:47 +1100

    error

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="main">
-
+    hi there
   </div>
 </template>
 
 <script>
+// const wappalyzer = require('@wappalyzer/wappalyzer');
+// const wappalyzer = require('wappalyzer');
+// import wappalyzer from 'wappalyzer';
+import wappalyzer from '@wappalyzer/wappalyzer';
+
 export default {
   data() {
     return {
